967af53efcd8d46a8024228de3b48e528cce4347
ducanhdev <ducanh.nguyen062003@gmail.com>
2025-06-23 08:45:53 +0700

    Add cart functionality with views, URLs, and templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store' #django app
+    'store', #django app
+    'cart', #django app
 ]
 
 MIDDLEWARE = [
--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.store, name='store'),
     path('product/<slug:slug>/', views.product_info, name='product_info'),
-    path('category/<slug:category_slug>/', views.list_category, name='list_category')
+    path('search/<slug:category_slug>/', views.list_category, name='list_category')
 ]
