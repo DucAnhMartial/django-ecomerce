80aac351805c566540bed24131b9d33b7ae5d3a1
ducanhdev <ducanh.nguyen062003@gmail.com>
2025-06-26 06:04:40 +0700

    add delete functionality

--- cart/views.py
+++ cart/views.py
@@ -33,7 +33,22 @@ def cart_add (request):
     return response
 
 def cart_delete (request):
-  pass
+  
+  cart = Cart(request)
+  
+  print(request.POST)
+  
+  if(request.POST.get('action') == 'post'):
+    
+    product_id = int(request.POST.get('product_id'))
+    
+    cart.delete(product = product_id)
+    
+    cart_quantity = cart.__len__()
+    
+    cart_total = cart.get_total()
+    
+    return JsonResponse({'cart_quantity': cart_quantity, 'cart_total': cart_total})
 
 
 def cart_update (request):
